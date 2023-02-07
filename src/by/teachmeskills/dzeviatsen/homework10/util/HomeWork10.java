0d66ebc9335df91c7e6443f1967c820453cfb7df
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-02-07 15:32:47 +0300

    ff

--- src/by/teachmeskills/dzeviatsen/homework10/util/HomeWork10.java
+++ src/by/teachmeskills/dzeviatsen/homework10/util/HomeWork10.java
@@ -1,12 +1,59 @@
 package by.teachmeskills.dzeviatsen.homework10.util;
 
+import java.util.Scanner;
+
 public class HomeWork10 {
     public static void main(String[] args) {
-        System.out.println(StringUtils.deleteExtraSpace("                    j iih   i     j            "));
-        System.out.println(StringUtils.getSecureNum("1234567891234567"));
-        System.out.println(StringUtils.getShortName("Daniil", "Dzeviatsen", "Pavlovich"));
-        System.out.println(StringUtils.ifCorrectRBPassportNumber("MC1234567"));
-        System.out.println(StringUtils.ifSafe("123asSwkfkwef"));
+        System.out.println("Enter task number");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        switch (number) {
+            case 1 -> {
+                System.out.println("Enter the row you want to be corrected");
+                String string = StringUtils.Input();
+                System.out.println("Your corrected row:");
+                System.out.println(StringUtils.deleteExtraSpace(string));
+            }
+            case 2 -> {
+                System.out.println("Enter the number you want to become secure");
+                String string = StringUtils.Input();
+                StringUtils.deleteExtraSpace(string);
+                System.out.println(StringUtils.getSecureNum(string));
+            }
+            case 3 -> {
+                System.out.println("Enter name");
+                String name = StringUtils.Input();
+                StringUtils.deleteExtraSpace(name);
+                System.out.println("Enter surname");
+                String surName = StringUtils.Input();
+                StringUtils.deleteExtraSpace(surName);
+                System.out.println("Enter middlename");
+                String middleName = StringUtils.Input();
+                StringUtils.deleteExtraSpace(middleName);
+                System.out.println("Your short name is");
+                System.out.println(StringUtils.getShortName(name, surName, middleName));
+            }
+            case 4 -> {
+                System.out.println("Enter passport number to check");
+                String string = StringUtils.Input();
+                StringUtils.deleteExtraSpace(string);
+                System.out.println("Is this correct passport number?");
+                System.out.println(StringUtils.ifCorrectRBPassportNumber(string));
+            }
+            case 5 -> {
+                System.out.println("Enter the password you want to check");
+                String string = StringUtils.Input();
+                System.out.println("is your password safe?");
+                System.out.println(StringUtils.ifSafe(string));
+            }
+            case 6 -> {
+                System.out.println("enter e-mail adress");
+                String string = StringUtils.Input();
+                System.out.println("is it an e-mail adress?");
+                System.out.println(StringUtils.ifEmailAdress(string));
+            }
+            default -> System.out.println("enter the number from 1 to 6");
+        }
     }
 
 }
--- src/by/teachmeskills/dzeviatsen/homework10/util/StringUtils.java
+++ src/by/teachmeskills/dzeviatsen/homework10/util/StringUtils.java
@@ -1,6 +1,6 @@
 package by.teachmeskills.dzeviatsen.homework10.util;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class StringUtils {
     public static String deleteExtraSpace(String string) {
@@ -12,9 +12,16 @@ public class StringUtils {
     }
 
     public static String getSecureNum(String string) {
+        if (string.length() != 16) {
+            throw new IllegalArgumentException("Enter correct number which contains 16 numerals");
+        }
         char[] chars = string.toCharArray();
-        for (int i = 12; i < chars.length; i++) {
-            chars[i] = '*';
+        for (int i = 0; i < chars.length - 4; i++) {
+            if (chars[i] >= '0' && chars[i] <= '9') {
+                chars[i] = '*';
+            } else {
+                throw new IllegalArgumentException("Enter correct number which contains 16 numerals");
+            }
         }
 
         string = String.valueOf(chars);
@@ -28,11 +35,11 @@ public class StringUtils {
     }
 
     public static String getShortName(String name, String surName, String middleName) {
-        name = name.substring(0, 1) + ".";
+        name = name.substring(0, 1).toUpperCase() + ".";
         if (middleName != "") {
-            middleName = middleName.substring(0, 1) + ".";
+            middleName = middleName.substring(0, 1).toUpperCase() + ".";
         }
-        String shortName = surName + " " + name + middleName;
+        String shortName = surName.substring(0, 1).toUpperCase() + surName.substring(1) + " " + name + middleName;
         return shortName;
     }
 
@@ -45,6 +52,8 @@ public class StringUtils {
                         tr = true;
                     }
                 }
+            } else {
+                tr = false;
             }
         }
         return tr;
@@ -52,22 +61,63 @@ public class StringUtils {
 
     public static boolean ifSafe(String string) {
         boolean tr = false;
-        if (string.length() < 9) {
+        if (string.length() < 8) {
             return tr;
         }
         for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i)>='0' && string.charAt(i)<='9'){
-                for (int j=0; j<string.length();j++){
-                    if (string.charAt(j)>='a'&&string.charAt(j)<='z'){
-                        for (int k=0;k<string.length();k++){
-                            if (string.charAt(k)>='A'&&string.charAt(k)<='Z'){
-                                tr=true;
+            if (string.charAt(i) >= '0' && string.charAt(i) <= '9') {
+                for (int j = 0; j < string.length(); j++) {
+                    if (string.charAt(j) >= 'a' && string.charAt(j) <= 'z') {
+                        for (int k = 0; k < string.length(); k++) {
+                            if (string.charAt(k) >= 'A' && string.charAt(k) <= 'Z') {
+                                tr = true;
+                            } else {
+                                continue;
                             }
                         }
+                    } else {
+                        continue;
                     }
                 }
+            } else {
+                continue;
             }
-        }return tr;
+        }
+        return tr;
     }
+
+    public static boolean ifEmailAdress(String string) {
+        boolean tr = false;
+        int counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ') {
+                if (string.charAt(i) == '@') {
+                    counter += 1;
+                    if (counter != 1) {
+                        tr = false;
+                        continue;
+                    }
+                    if (i != string.length() - 1 && i != 0) {
+                        tr = true;
+                    } else {
+                        tr = false;
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+        }
+        return tr;
+    }
+
+    public static String Input() {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        return string;
+    }
+
 }
 
